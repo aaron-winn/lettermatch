20f43248279f32b51b30af7bf1a9aaf1fec57f92
Aaron_Winn <winn403@hotmail.com>
2017-12-11 10:28:13 -0700

    first

---
+++ letter-match.rb
@@ -0,0 +1,23 @@
+def enter_word
+    phrase = gets.chomp
+
+#turn each word array
+#
+
+    arr.each do |w|
+        word = w.length w.split(' ')
+            if word 
+    
+                
+           
+
+            end
+    end 
+# check for duplicate letters
+#count to see what word has most
+lw_match = 
+# Display word with most
+   
+puts lw_match
+
+end    
